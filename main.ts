62d0d32a343aa58734c273694b85101b26d63a7d
youpong <43817652+youpong@users.noreply.github.com>
2025-12-17 01:15:26 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-let lum = 0
-let sat = 0
 let hue = 0
+let sat = 0
+let lum = 0
+
 let strip = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
 let range = strip.range(0, 1)
 basic.forever(function () {
